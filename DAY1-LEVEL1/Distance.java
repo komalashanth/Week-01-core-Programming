79d0a4d23121bedbd6061799e6c3bd26e6078492
komalashanth <komalashanthithota@gmail.com>
2025-04-03 16:04:11 +0530

    delete file day1 level1

--- DAY1-LEVEL1/Distance.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Distance{
-public static void main(String[] args){
-double distance=10.8;
-double miles=distance*1.6;
-System.out.println("The distance 10.8 km in miles is" + miles);
-}
-}
