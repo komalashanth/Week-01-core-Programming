c99811025d259b4fc7a6e46094255a206e303927
komalashanth <komalashanthithota@gmail.com>
2025-04-03 16:34:47 +0530

    add day3 level 1

---
+++ DAY3-LEVEL1/NumberCheck.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < 5; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            if (numbers[i] > 0) {
+                if (numbers[i] % 2 == 0) {
+                    System.out.println(numbers[i] + " is positive and even.");
+                } else {
+                    System.out.println(numbers[i] + " is positive and odd.");
+                }
+            } else if (numbers[i] < 0) {
+                System.out.println(numbers[i] + " is negative.");
+            } else {
+                System.out.println(numbers[i] + " is zero.");
+            }
+        }
+
+        if (numbers[0] > numbers[4]) {
+            System.out.println("The first number is greater than the last number.");
+        } else if (numbers[0] < numbers[4]) {
+            System.out.println("The first number is less than the last number.");
+        } else {
+            System.out.println("The first and last numbers are equal.");
+        }
+    }
+}
